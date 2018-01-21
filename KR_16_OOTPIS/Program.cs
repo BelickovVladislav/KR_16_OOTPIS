4af70e7981b22c86cd5f7a6cb3d0ec7a3aa5cace
Belikov Vladislav <belickov.vlad1997@yandex.ru>
2018-01-21 15:40:52 +0300

    Добавьте файлы проекта.

---
+++ KR_16_OOTPIS/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KR_16_OOTPIS
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
